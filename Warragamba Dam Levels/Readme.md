402c8ce36e706cbc803b393d7eb5fb17db7078b0
Andrew Turner <andrew_turner97@hotmail.com>
2023-07-17 21:43:50 +1000

    Setup Base Repo

---
+++ Warragamba Dam Levels/Readme.md
@@ -0,0 +1,10 @@
+A friend of mine mentioned that WaterNSW updated their website.
+However, like most web facelifts, the websites's functionality took a hit.
+
+As a policy maker hes very interested in the daily releases of dam levels as well as the historic dataset.
+Rather than him visiting the site each day and recording the data manually I wrote this basic script with github actions to scrape the site every day then kick it out as a csv.
+
+All these daily data extracts are avaiable in the data folder.
+If you want a specific daterange 
+
+Open sourcing (MIT Licence) for those who are interested in dam levels!
---
+++ Warragamba Dam Levels/damlevel_scrape.r
@@ -0,0 +1,51 @@
+library(tidyverse)
+
+#Define the Site and the filter Criteria
+url <- "https://www.waternsw.com.au/nsw-dams/nsw-storage-levels/greater-sydney-dam-levels"
+dams_of_interest <- c("Avon Dam",
+                      "Blue Mountains Dams",
+                      "Cataract Dam",
+                      "Cordeaux Dam",
+                      "Fitzroy Falls Reservoir",
+                      "Nepean Dam",
+                      "Prospect Reservoir",
+                      "Tallowa Dam",
+                      "Warragamba Dam",
+                      "Wingecarribee Reservoir",
+                      "Woronora Dam"
+                      )
+#Scrape Scrape Scrape
+page <- xml2::read_html(url)
+
+Dam_Level <- page %>%
+  rvest::html_nodes("div.box") %>%  
+  rvest::html_nodes("span.percentage")%>% 
+  rvest::html_text()
+
+
+Dam_Name <- page %>% 
+  rvest::html_nodes("div.box") %>% 
+  rvest::html_nodes("h3") %>%  
+  rvest::html_text() 
+
+Sample_Level <- data.frame(Dam_Name) %>% 
+    filter(Dam_Name %in% dams_of_interest) %>% 
+    cbind(Dam_Level) %>% 
+    mutate(Data_Collection_Time_AEST = Sys.time()) %>% 
+    mutate(Collection_Date = Sys.Date())
+
+#Beep boop out goes data
+write.csv(Sample_Level, paste0("data/",Sys.Date(),"_GSDL.csv"), row.names = FALSE)
+
+#Nom nom ingest the data
+filenames <- paste0("data/", list.files("data/"))
+
+list_data <- lapply(filenames, read.csv)
+
+#Ingest the data !!
+master_data <- do.call("rbind",list_data)
+
+#Output the new masterdata:
+#Should output around 4k rows a year - will hit the excel max in 256 years on daily data polls - 
+# If this overflows and drops starts to drop data you've relied on the scraper for too long LOL
+write.csv(master_data, ("Daily_Dam_Readings_Greater_Sydney.csv"), row.names = FALSE)
