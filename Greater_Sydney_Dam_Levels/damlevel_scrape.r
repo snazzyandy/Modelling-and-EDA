d70725610aebc8856da274b573c71b0df7cba0e9
Andrew Turner <andrew_turner97@hotmail.com>
2023-07-21 17:26:12 +1000

    Updated codebase to include headline stats
    Added;
    daily rain (mm)
    total levels (%)
    daily consumption(ML)

--- Greater_Sydney_Dam_Levels/damlevel_scrape.r
+++ Greater_Sydney_Dam_Levels/damlevel_scrape.r
@@ -18,22 +18,35 @@ dams_of_interest <- c("Avon Dam",
 #Scrape Scrape Scrape
 page <- xml2::read_html(url)
 
-Dam_Level <- page %>%
+system_level <- page %>%
+  rvest::html_nodes("div.box") %>%
+  rvest::html_nodes("div.levels-box") %>%  
+  rvest::html_nodes("div.number") %>%
+  rvest::html_text() 
+
+Daily_Headline_Figures <- data.frame(Variable= c("How Much Rain have We had?", "How Much Water is in Our Damns?", "How Much Water have we Used"),
+                  daily_value = system_level) %>% 
+                  mutate(Data_Collection_Time_UTC = format(Sys.time(), tz="UTC")) %>% 
+                  mutate(Collection_Date = Sys.Date())
+    
+
+daily_value <- page %>%
   rvest::html_nodes("div.box") %>%  
   rvest::html_nodes("span.percentage")%>% 
   rvest::html_text()
 
 
-Dam_Name <- page %>% 
+Variable <- page %>% 
   rvest::html_nodes("div.box") %>% 
   rvest::html_nodes("h3") %>%  
   rvest::html_text() 
 
-Sample_Level <- data.frame(Dam_Name) %>% 
-    filter(Dam_Name %in% dams_of_interest) %>% 
-    cbind(Dam_Level) %>% 
+Sample_Level <- data.frame(Variable) %>% 
+    filter(Variable %in% dams_of_interest) %>% 
+    cbind(daily_value) %>% 
     mutate(Data_Collection_Time_UTC = format(Sys.time(), tz="UTC")) %>%
-    mutate(Collection_Date = Sys.Date())
+    mutate(Collection_Date = Sys.Date()) %>% 
+    rbind(Daily_Headline_Figures)
 
 #Beep boop out goes data
 write.csv(Sample_Level, paste0("data/",format(Sys.time(),  "%Y%m%d_%H%M%S_"),"_GSDL.csv"), row.names = FALSE)
